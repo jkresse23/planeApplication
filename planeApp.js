74abfc448d35a56c043ca1fd44036d0812098bd3
jkresse23 <kresse.john@gmail.com>
2016-08-17 19:46:13 -0400

    Add files via upload
    This is the final version of the app.  The user can check the status of their flight, or add a new flight that is stored as an object in an array called flights.  They can also see all the flights (via ng-repeat and ng-show) if they so desire.

--- planeApp.js
+++ planeApp.js
@@ -9,62 +9,84 @@ now I am pulling object data into my view from the controller and I am pulling t
 flight number they enter 
 
 
-what I'm trying to do in line 40 and on is let the user add a new flight number, but right now I can't figure out how to
-add a new flight object to my existing list of flight objects.*/
+I am trying to display all flights.  Right now I can display the last flight, but need
+to figure out how to display the entire array. From there, it will be fairly easy to see
+if I've added the new flight to my existing array of flights.
+
+Let's start by adding a new flight to flights.  I think I need to create a new object
+before adding it to the flights array  
+
+Each time the user adds a flight, it's being added to the array of flights which is great.
+Problem is it's also changing the previous submission's values to those new values, so 
+we're getting multiple records of the same values.  I need to add a new object, store that 
+object, and get a new object in there.
+
+*/
 
 var app = angular.module("app", []);
 
 app.controller("Controller", function() {
 	
-	this.flights = {
-		1 : {status : "is delayed. I'm so sorry!"},
-		2 : {status : "is right on time.  Lucky you!"},
-		7 : {status : "already left.  Where were you?"}
+	this.flights = [
+		{flightID : "1", status : "is delayed. I'm so sorry!"},
+		{flightID : "2", status : "is right on time.  Lucky you!"},
+		{flightID : "7", status : "already left.  Where were you?"},
+		{flightID : "12", status : "is stuck on the tarmack at LGA."}
+		
+		];
+		
 	
-	};
-
-
 	this.userSubmit = function(flightNumber) {
-	
 		
 		this.flightUpdate = "Going through.";
 		
 		this.intro = "Flight #";
 		
+		for (this.i = 0; this.i < this.flights.length; this.i++) {
+
+			
+			if (this.flights[this.i].flightID === this.flightNumber) {
+				
+				console.log("Match!");
+				
+				this.flightStatus = this.flights[this.i].status;
+				
+				return "";
+				
+				} else {
+				
+				console.log("No Match!");
+				
+				this.flightStatus = this.flightNumber + " does not exist in your system.  Please re-enter your flight number.";
+				
+				}			
+			};		
+		
+		};
+		
+		this.addNewFlight = function (newFlightNumber, newFlightStatus) {
+	
+			this.newFlight = {};
+			
+			this.newFlight.flightID = this.newFlightNumber;
+			
+			this.newFlight.status = this.newFlightStatus;
+			
+			this.newFlightDisplayed = "You've just added flight #" + this.newFlightNumber + ", which you've indicated " + 
+				
+				this.newFlightStatus;
+				
+			this.flights.push(this.newFlight);
+			
+			console.log(this.flights);
+			
+				
+	}; 
+		
 		this.usersFlight = this.flightNumber;
 		
-		this.flightStatus = this.flights[this.usersFlight].status;
-		
 	
-	};
-	
-/*	this.addNewFlight = function (newFlightNumber, newFlightStatus) {
-	
-		this.newFlight = this.flights.add[this.newFlightNumber];
-		
-		this.updatedListOfFlights = this.flights;
-	
-	}; */
 
 });
 
-/* app.factory("Data", function() {
 
-	
-	return flights = {
-		001 : {status : "is delayed. I'm so sorry!"},
-		002 : {status : "is right on time.  Lucky you!"},
-		003 : {status : "already left.  Where were you?"}
-	
-	};
-});
-	
-
-var categories = {
-	bakeries : ["bakery1", "bakery2"],
-	 coffeeShops : ["coffeeshop1", "coffeeshop2"]
-	 };
-
-console.log(categories);
-
-*/
